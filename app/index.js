96578ab0e15b963d049d8277ba177327ec438aab
Alex Querins <querins777@gmail.com>
2017-08-14 16:35:59 +0300

    Webpack config rewritten

--- app/index.js
+++ app/index.js
@@ -1,1 +1,30 @@
 import "./style.scss";
+
+var Vue = require('vue/dist/vue.js');
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    posts: [],
+    input: ''
+  },
+  methods: {
+    add: function(event) {
+      if (this.isInputValid) {
+        this.posts.push({title: this.input});
+        this.input = '';
+      }
+    }
+  },
+  computed: {
+    matchedPosts: function() {
+      let phrase = this.input;
+      return this.posts.filter(function(post, index, posts) {
+        return post.title.includes(phrase);
+      });
+    },
+    isInputValid: function() {
+      return this.input !== '';
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,79 @@
-const path = require('path');
-
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const WriteFilePlugin = require('write-file-webpack-plugin');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './app/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build/')
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/build/',
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        loader: "babel-loader",
+        test: /\.vue$/,
+        loader: 'vue-loader',
         options: {
-          presets: ["env"]
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+          // other vue-loader options go here
         }
       },
       {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-       {
-         test: /\.(csv|tsv)$/,
-         use: [
-           'csv-loader'
-         ]
-       },
-       {
-         test: /\.xml$/,
-         use: [
-           'xml-loader'
-         ]
-       }
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
     ]
   },
-  plugins: [
-      new ExtractTextPlugin({
-        filename: "bundle.css"
-      }),
-      new WriteFilePlugin()
-  ]
-};
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
